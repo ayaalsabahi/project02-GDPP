12f40b62e0f45b26fccc1ff656e3c6ffc30d2853
Dimitrios <dimitriosbroumas@gmai.com>
2024-03-17 16:05:04 -0400

    Update BoxController.cs

--- Assets/Scripts/BoxController.cs
+++ Assets/Scripts/BoxController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class BoxController : MonoBehaviour, Interactable
 {
     public BoxCollider2D collider;
+    public Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
         collider = gameObject.GetComponent<BoxCollider2D>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,7 +21,15 @@ public class BoxController : MonoBehaviour, Interactable
 
     public IEnumerator Interact()
     { 
-        Debug.Log("tuuu");
+        if(collider.enabled == true)
+        {
+            collider.enabled = false;
+            rb.gravityScale = 0;
+        }
+        else{
+            collider.enabled = true;
+            rb.gravityScale = 1;
+        }
         yield break;
     }
 }
