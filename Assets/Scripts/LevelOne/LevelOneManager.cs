e360cb7051e03db6d72ab3fc14d17fb39a8a8d59
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-03-19 22:23:37 -0400

    added scene switch + door unlocking

--- Assets/Scripts/LevelOne/LevelOneManager.cs
+++ Assets/Scripts/LevelOne/LevelOneManager.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 
 public class LevelOneManager : MonoBehaviour
 {
+    bool isPinFound = false;
+    public GameObject pinButton;
+    public GameObject doorToLift; 
+    //create a 2d sprite thing that is initially off 
+    private void Start()
+    {
+        pinButton.SetActive(false);
+    }
 
-    
+
+    public void pinFound()
+    {
+        isPinFound = true;
+        pinButton.SetActive(true);
+
+    }
+
+    public void exitedPin()
+    {
+        if (isPinFound)
+        {
+            LeanTween.moveLocalY(doorToLift, transform.localPosition.y + 0.1f, 2f)
+            .setEase(LeanTweenType.easeOutQuad);
+        }
+    }
+
+
 }
